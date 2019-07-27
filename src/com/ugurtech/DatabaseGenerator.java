845ab7099f761dee2edb6cf70a37f70e6f06bf62
Halil UĞUR <halil__ugur@hotmail.com>
2019-07-27 12:15:02 +0300

    first commit.

---
+++ src/com/ugurtech/DatabaseGenerator.java
@@ -0,0 +1,13 @@
+package com.ugurtech;
+
+/**
+ * @author Halil UĞUR
+ * @version 1.0
+ * @since 27.07.2019
+ */
+public class DatabaseGenerator {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Word");
+    }
+}
